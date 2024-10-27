b81801a1d3d4a1aa6ba0afca648987b70bfd47f0
manduz <emekmanduz3623@gmail.com>
2024-10-28 00:07:04 +0300

    App Config ile sql bağlantısı 3. yol

---
+++ AppConfig/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConfig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection sqlcon = new SqlConnection();
+
+            //2. adım 
+            sqlcon.ConnectionString = ConfigurationManager.ConnectionStrings["SQLSQL"].ConnectionString;
+            sqlcon.Open();
+            Console.WriteLine($"bağlantı: {sqlcon.State}");
+            sqlcon.Close();
+            Console.WriteLine($"bağlantı: {sqlcon.State}");
+            Console.ReadLine();
+        }
+    }
+}
