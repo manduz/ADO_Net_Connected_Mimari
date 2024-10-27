eaad08df724fb61eabce9db87b0646f4b5ed4302
manduz <emekmanduz3623@gmail.com>
2024-10-28 00:31:17 +0300

    Bağlantı kontrolunü farklı yöntemle bulma

---
+++ Baglanti_Kontrol/DataAccessLayer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baglanti_Kontrol
+{
+    internal class DataAccessLayer
+    {
+        SqlConnection connect;
+        public DataAccessLayer() //constructer
+        {
+            connect = new SqlConnection();
+            connect.ConnectionString = ConfigurationManager.ConnectionStrings["emek"].ConnectionString;
+
+        }
+        void BaglantiYonetim()
+        {
+            if (connect.State == System.Data.ConnectionState.Open)
+            {
+                connect.Close();
+            }
+            else
+                connect.Open();
+        }
+        public void TestBaglanti()
+        {
+            BaglantiYonetim();
+            Console.WriteLine($"Bağlantı duurmu: {connect.State}");
+            BaglantiYonetim();
+            Console.WriteLine($"Bağlantı duurmu: {connect.State}");
+        }
+    }
+}
---
+++ Baglanti_Kontrol/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baglanti_Kontrol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataAccessLayer dataAccessLayer = new DataAccessLayer();
+            dataAccessLayer.TestBaglanti();
+            Console.ReadLine();
+        }
+    }
+}
