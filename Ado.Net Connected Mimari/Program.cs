e0e7d0d4d980cbe6e978c10e59859c3aa3b2a97c
manduz <emekmanduz3623@gmail.com>
2024-10-27 21:54:42 +0300

    sql ile bağlantı ayarı

---
+++ Ado.Net Connected Mimari/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ado.Net_Connected_Mimari
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection sqlcon = new SqlConnection();
+            sqlcon.ConnectionString = "Data Source=DESKTOP-CU1M14V\\SQLKODLAB;Initial Catalog=SQLFULL;User ID=udemy1;Encrypt=False;Password=1";
+            sqlcon.Open();//C# ile sql server bağlantısını açar
+            Console.WriteLine($"Bağlantı Durumu: {sqlcon.State}");
+            sqlcon.Close();
+            Console.WriteLine($"Bağlantı Durumu: {sqlcon.State}");
+            Console.ReadLine();
+
+
+        }
+    }
+}
